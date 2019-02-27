245448febefb1b9b35beaa6ec3c1a547abb710ae
Julien B <julien@toshokan.fr>
2019-02-27 17:15:57 +0100

    Reorganized cached.py

--- cached/cached.py
+++ cached/cached.py
@@ -6,18 +6,22 @@ from functools import wraps, partial
 from hashlib import md5
 from inspect import getmodule
 
+from redis import Redis
 from redis.exceptions import TimeoutError, AuthenticationError, ConnectionError
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(levelname)s %(message)s')
 
 
-def cached(func=None, timeout: int=60, redis_attribute: str='cache', key_prefix: str=None):
+def cached(func=None, redis_host: str = 'localhost', timeout: int = 60,
+           redis_port=6379, redis_db=0, key_prefix: str = None):
     """
     Decorator function to add Redis caching capability to functions
+    :param redis_host: (str) IP or hostname where Redis is installed
     :param func: (function) the decorated function
     :param timeout: (int) the Redis timeout/TTL (default: 60 secs)
-    :param redis_attribute: (str) the name of the redis client attribute
+    :param redis_port: (int) the Redis TCP port (default: 6379)
+    :param redis_db: (int) the Redis DB to use (default: 0)
     :param key_prefix: (str) a prefix for the redis keys (optional)
     :return: reference to the decorator function
     """
@@ -28,7 +32,7 @@ def cached(func=None, timeout: int=60, redis_attribute: str='cache', key_prefix:
     if not func:
         return partial(cached,
                        timeout=timeout,
-                       redis_attribute=redis_attribute,
+                       redis_host=redis_host,
                        key_prefix=key_prefix)
 
     # Will be used for the Redis key construction
@@ -49,8 +53,8 @@ def cached(func=None, timeout: int=60, redis_attribute: str='cache', key_prefix:
         args_key = md5(pickle.dumps(args_tuple)).hexdigest()
         redis_key = f'{key_prefix or ""}_cache_{module_name}.{func_name}.{args_key}'
 
-        # Get the Redis client store in redis_attribute
-        redis = getattr(service, redis_attribute)
+        # Get the Redis client
+        redis = Redis(host=redis_host, port=redis_port, db=redis_db)
 
         try:
             # If the key already exists in Redis, we use it to retrieve the
@@ -85,4 +89,3 @@ def cached(func=None, timeout: int=60, redis_attribute: str='cache', key_prefix:
             return response
 
     return decorated_function
-
